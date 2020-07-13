ae64739551006bd0a69a1bd35e642c64568499f6
astridlyre <51189118+astridlyre@users.noreply.github.com>
2020-07-13 14:51:55 -0400

    website name

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -49,7 +49,7 @@
               <polyline points="15 3 21 3 21 9"></polyline>
               <line x1="10" y1="14" x2="21" y2="3"></line>
             </svg>
-            <span class="ml-2">{{ profileUser.website }}</span></a
+            <span class="ml-2">My Awesome Website</span></a
           >
         </div>
       </div>
